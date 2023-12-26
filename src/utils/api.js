bb1b240bd8860e5080fe213487187de758a13a1d
shaifali <shaifalikatiyar428@gmail.com>
2023-12-26 23:33:40 +0530

    API tested successfully

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,21 @@
 import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3";
-const TOKEN = import.meta.env.VITE_APP_TOKEN;
-const headers={
-    Authorization: "bearer " + TOKEN,
+const TOKEN =
+	"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjYxMmQzZDNkNzNlMGZmMjk5YTZjZjBkYTI3Yzk1MiIsInN1YiI6IjY1Nzc1NDFiZWM4YTQzMDBmZDdkYzkyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rR0Iva9J0bMbkPUnn5sJgnLCGk4lpVLu8dW71gUAuGI";
+const headers = {
+	authorization: "bearer " + TOKEN,
 };
- const fetchDataFromApi =async (url, params)=>{
-    try{
-
-        const {response} = await axios.get(BASE_URL+url, {
-            headers: headers,
-            params: params
-        })
-        return response;
-    }
-    catch(error){
-        console.log(error);
-        return error;
-    }
-}
+const fetchDataFromApi = async (url, params) => {
+	try {
+		const { data } = await axios.get(BASE_URL + url, {
+			headers: headers,
+			params: params,
+		});
+		return data;
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+};
 
 export default fetchDataFromApi;
-
-

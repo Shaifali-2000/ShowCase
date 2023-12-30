f6a8ae666fd41ba0b583d31e10cf6c8a079f06d7
shaifali <shaifalikatiyar428@gmail.com>
2023-12-30 16:52:45 +0530

    Added React Router successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState, useEffect } from "react";
 import fetchDataFromAPI from "./utils/api";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { useSelector, useDispatch } from "react-redux";
 import { getApiconfiguration } from "./store/homeSlice";
+
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import Error404 from "./pages/404/Error404";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 function App() {
 	const dispatch = useDispatch();
 	const { url } = useSelector((state) => state.home.url);
@@ -17,7 +27,19 @@ function App() {
 		testing();
 	}, []);
 
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/:mediaType/:id" element={<Details />} />
+				<Route path="/search/:query" element={<SearchResult />} />
+				<Route path="/explore/:mediaType" element={<Explore />} />
+				<Route path="*" element={<Error404 />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;

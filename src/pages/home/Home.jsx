8d56a6f20977e28d03b8ea3c4376e7f2e859e8d8
shaifali <shaifalikatiyar428@gmail.com>
2024-01-04 22:37:38 +0530

    Added Footer And SwitchTabs successfully

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+
 import "./home.scss";
 
+import Trending from "./trending/Trending";
 import HeroBanner from "./heroBanner/HeroBanner";
 
 const Home = () => {
-	return <HeroBanner />;
+	return (
+		<div className="homePage">
+			<HeroBanner />
+			<Trending />
+		</div>
+	);
 };
 
 export default Home;
